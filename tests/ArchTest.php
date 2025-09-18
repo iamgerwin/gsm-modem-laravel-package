63959e3c536e7032bc44dde05ea46475fc19c2d8
John Gerwin De las Alas <iamgerwin@live.com>
2025-09-18 17:23:43 +0800

    fix: remove namespaces from Pest functional tests
    Pest functional tests should not have namespace declarations
    as they use the global namespace by design

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-namespace Iamgerwin\GsmModem\Tests;
-
 arch('it will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-namespace Iamgerwin\GsmModem\Tests;
-
 it('can test', function () {
     expect(true)->toBeTrue();
 });
